17d72d55b2b128af374827825a821c99bad7e7e2
Veelkoov <veelkoov@protonmail.com>
2023-10-23 18:59:12 +0200

    Divided test

--- src/test/kotlin/species/SpeciesLoaderTest.kt
+++ src/test/kotlin/species/SpeciesLoaderTest.kt
@@ -12,14 +12,23 @@ class SpeciesLoaderTest {
             setOf("Most species", "Other", "Third root"),
             specieNamesSet(subject.getAsTree()),
         )
+    }
 
+    @Test
+    fun `Third root has no connections`() {
         assertEquals(0, subject.getByName("Third root").getParents().size)
         assertEquals(0, subject.getByName("Third root").getChildren().size)
+    }
 
+    @Test
+    fun `Exception thrown for nonexistent specie`() {
         assertFailsWith<SpecieException> {
             subject.getByName("Nonexistent")
         }
+    }
 
+    @Test
+    fun `Family properly constructed`() {
         assertEquals(
             setOf("Felines", "Panthers", "Deer", "Some deer specie"),
             specieNamesSet(subject.getByName("Mammals").getDescendants()),
